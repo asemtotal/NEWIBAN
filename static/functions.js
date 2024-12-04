58dea868002b1d82bc045b3fd7021188b4dd0699
asemtotal <dssaads08@gmail.com>
2024-12-04 19:04:03 +0200

    Update functions.js

--- static/functions.js
+++ static/functions.js
@@ -1,3 +1,15 @@
+function getBasePath() {
+    const currentPath = window.location.pathname;
+    const pathSegments = currentPath.split('/');
+    // تحديد المسار الأساسي بناءً على موقع "index.html"
+    return pathSegments.slice(0, pathSegments.length - 1).join('/') + '/';
+}
+
+function updateUrl(url_extension) {
+    const basePath = getBasePath();
+    window.history.pushState(null, '', basePath + url_extension);
+}
+
 function display() {
     var e = document.getElementById("country_input").value;
     updateUrl('?country=' + e);
@@ -16,28 +28,22 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function updateUrl(url_extension){
-    window.history.pushState(null, '', '/NEWIBAN/' + url_extension);
-}
-
 $('document').ready(function() {
-
     var sel = document.getElementById('country_input');
     for (var i = 0; i < countries.length; i++) {
         var opt = document.createElement('option');
         opt.innerHTML = countries[i].replace(/_/g, " ");
         opt.value = countries[i];
-        opt.id = "country_"+countries[i];
+        opt.id = "country_" + countries[i];
         sel.appendChild(opt);
     }
     var active_country = null;
-    if (getParameterByName("country") === null){
+    if (getParameterByName("country") === null) {
         active_country = "Netherlands";
         updateUrl('?country=Netherlands');
-    }
-    else{
+    } else {
         active_country = getParameterByName("country");
     }
     var x = document.getElementById("demo").innerHTML = buildIbans(active_country);
-    document.getElementById("country_"+active_country).selected=true;
+    document.getElementById("country_" + active_country).selected = true;
 });
